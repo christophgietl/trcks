9eb51c70c05d1d2c36d9966abc0d74c2ca474911
Christoph Gietl <christophgietl@users.noreply.github.com>
2025-02-26 21:38:38 +0100

    Add (Awaitable)Result to trcks.oop

--- src/trcks/oop/__init__.py
+++ src/trcks/oop/__init__.py
@@ -1,11 +1,13 @@
-from trcks.oop.async_dual_track import AsyncDualTrack
+from trcks.oop.async_dual_track import AsyncDualTrack, AwaitableResult
 from trcks.oop.async_single_track import AsyncSingleTrack
-from trcks.oop.dual_track import DualTrack
+from trcks.oop.dual_track import DualTrack, Result
 from trcks.oop.single_track import SingleTrack
 
 __all__ = [
     "AsyncDualTrack",
     "AsyncSingleTrack",
+    "AwaitableResult",
     "DualTrack",
+    "Result",
     "SingleTrack",
 ]
