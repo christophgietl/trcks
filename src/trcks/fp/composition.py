07a5aa22552738be98cd7fb06cfb736e351c4d68
Christoph Gietl <christophgietl@users.noreply.github.com>
2025-02-28 19:54:55 +0100

    Refactor module composition

--- src/trcks/fp/composition.py
+++ src/trcks/fp/composition.py
@@ -1,20 +1,16 @@
-"""Higher order functions for composition.
+"""Higher order functions for composition."""
 
-This module is heavily inspired by the expression library
-(see https://github.com/dbrattli/Expression/blob/a39d98592012e482df140af86b82a59c3d5ba4b7/expression/core/pipe.py).
-"""
+from __future__ import annotations
 
-import functools
 import sys
 from collections.abc import Callable
-from typing import Any, TypeVar, overload
+from typing import Optional, TypeVar, overload
 
 if sys.version_info >= (3, 11):
     from typing import TypeAlias
 else:
     from typing_extensions import TypeAlias
 
-_X = TypeVar("_X")
 _X0 = TypeVar("_X0")
 _X1 = TypeVar("_X1")
 _X2 = TypeVar("_X2")
@@ -26,242 +22,354 @@ _X7 = TypeVar("_X7")
 _X8 = TypeVar("_X8")
 _X9 = TypeVar("_X9")
 
-_Morphism: TypeAlias = Callable[[_X0], _X1]
-_Endomorphism: TypeAlias = Callable[[_X], _X]
+_F: TypeAlias = Callable[[_X0], _X1]
+_OF: TypeAlias = Optional[_F[_X0, _X1]]
+
+
+@overload
+def compose(
+    f1: _F[_X0, _X1],
+    f2: None = None,
+    f3: _OF[_X2, _X3] = None,
+    f4: _OF[_X3, _X4] = None,
+    f5: _OF[_X4, _X5] = None,
+    f6: _OF[_X5, _X6] = None,
+    f7: _OF[_X6, _X7] = None,
+    f8: _OF[_X7, _X8] = None,
+    f9: _OF[_X8, _X9] = None,
+) -> _F[_X0, _X1]: ...
+
+
+@overload
+def compose(
+    f1: _F[_X0, _X1],
+    f2: _F[_X1, _X2],
+    f3: None = None,
+    f4: _OF[_X3, _X4] = None,
+    f5: _OF[_X4, _X5] = None,
+    f6: _OF[_X5, _X6] = None,
+    f7: _OF[_X6, _X7] = None,
+    f8: _OF[_X7, _X8] = None,
+    f9: _OF[_X8, _X9] = None,
+) -> _F[_X0, _X2]: ...
+
+
+@overload
+def compose(
+    f1: _F[_X0, _X1],
+    f2: _F[_X1, _X2],
+    f3: _F[_X2, _X3],
+    f4: None = None,
+    f5: _OF[_X4, _X5] = None,
+    f6: _OF[_X5, _X6] = None,
+    f7: _OF[_X6, _X7] = None,
+    f8: _OF[_X7, _X8] = None,
+    f9: _OF[_X8, _X9] = None,
+) -> _F[_X0, _X3]: ...
+
+
+@overload
+def compose(
+    f1: _F[_X0, _X1],
+    f2: _F[_X1, _X2],
+    f3: _F[_X2, _X3],
+    f4: _F[_X3, _X4],
+    f5: None = None,
+    f6: _OF[_X5, _X6] = None,
+    f7: _OF[_X6, _X7] = None,
+    f8: _OF[_X7, _X8] = None,
+    f9: _OF[_X8, _X9] = None,
+) -> _F[_X0, _X4]: ...
+
+
+@overload
+def compose(
+    f1: _F[_X0, _X1],
+    f2: _F[_X1, _X2],
+    f3: _F[_X2, _X3],
+    f4: _F[_X3, _X4],
+    f5: _F[_X4, _X5],
+    f6: None = None,
+    f7: _OF[_X6, _X7] = None,
+    f8: _OF[_X7, _X8] = None,
+    f9: _OF[_X8, _X9] = None,
+) -> _F[_X0, _X5]: ...
+
+
+@overload
+def compose(
+    f1: _F[_X0, _X1],
+    f2: _F[_X1, _X2],
+    f3: _F[_X2, _X3],
+    f4: _F[_X3, _X4],
+    f5: _F[_X4, _X5],
+    f6: _F[_X5, _X6],
+    f7: None = None,
+    f8: _OF[_X7, _X8] = None,
+    f9: _OF[_X8, _X9] = None,
+) -> _F[_X0, _X6]: ...
+
+
+@overload
+def compose(
+    f1: _F[_X0, _X1],
+    f2: _F[_X1, _X2],
+    f3: _F[_X2, _X3],
+    f4: _F[_X3, _X4],
+    f5: _F[_X4, _X5],
+    f6: _F[_X5, _X6],
+    f7: _F[_X6, _X7],
+    f8: None = None,
+    f9: _OF[_X8, _X9] = None,
+) -> _F[_X0, _X7]: ...
+
+
+@overload
+def compose(
+    f1: _F[_X0, _X1],
+    f2: _F[_X1, _X2],
+    f3: _F[_X2, _X3],
+    f4: _F[_X3, _X4],
+    f5: _F[_X4, _X5],
+    f6: _F[_X5, _X6],
+    f7: _F[_X6, _X7],
+    f8: _F[_X7, _X8],
+    f9: None = None,
+) -> _F[_X0, _X8]: ...
+
+
+@overload
+def compose(
+    f1: _F[_X0, _X1],
+    f2: _F[_X1, _X2],
+    f3: _F[_X2, _X3],
+    f4: _F[_X3, _X4],
+    f5: _F[_X4, _X5],
+    f6: _F[_X5, _X6],
+    f7: _F[_X6, _X7],
+    f8: _F[_X7, _X8],
+    f9: _F[_X8, _X9],
+) -> _F[_X0, _X9]: ...
+
+
+def compose(  # noqa: PLR0911, PLR0913
+    f1: _F[_X0, _X1],
+    f2: _OF[_X1, _X2] = None,
+    f3: _OF[_X2, _X3] = None,
+    f4: _OF[_X3, _X4] = None,
+    f5: _OF[_X4, _X5] = None,
+    f6: _OF[_X5, _X6] = None,
+    f7: _OF[_X6, _X7] = None,
+    f8: _OF[_X7, _X8] = None,
+    f9: _OF[_X8, _X9] = None,
+) -> (
+    _F[_X0, _X1]
+    | _F[_X0, _X2]
+    | _F[_X0, _X3]
+    | _F[_X0, _X4]
+    | _F[_X0, _X5]
+    | _F[_X0, _X6]
+    | _F[_X0, _X7]
+    | _F[_X0, _X8]
+    | _F[_X0, _X9]
+):
+    if f2 is None:
+        return lambda x0: f1(x0)
+    if f3 is None:
+        return lambda x0: f2(f1(x0))
+    if f4 is None:
+        return lambda x0: f3(f2(f1(x0)))
+    if f5 is None:
+        return lambda x0: f4(f3(f2(f1(x0))))
+    if f6 is None:
+        return lambda x0: f5(f4(f3(f2(f1(x0)))))
+    if f7 is None:
+        return lambda x0: f6(f5(f4(f3(f2(f1(x0))))))
+    if f8 is None:
+        return lambda x0: f7(f6(f5(f4(f3(f2(f1(x0)))))))
+    if f9 is None:
+        return lambda x0: f8(f7(f6(f5(f4(f3(f2(f1(x0))))))))
+    return lambda x0: f9(f8(f7(f6(f5(f4(f3(f2(f1(x0)))))))))
 
 
 @overload
 def pipe(
-    value: _X0,
-    /,
+    x0: _X0,
+    f1: None = None,
+    f2: _OF[_X1, _X2] = None,
+    f3: _OF[_X2, _X3] = None,
+    f4: _OF[_X3, _X4] = None,
+    f5: _OF[_X4, _X5] = None,
+    f6: _OF[_X5, _X6] = None,
+    f7: _OF[_X6, _X7] = None,
+    f8: _OF[_X7, _X8] = None,
+    f9: _OF[_X8, _X9] = None,
 ) -> _X0: ...
 
 
 @overload
 def pipe(
-    value: _X0,
-    f1: _Morphism[_X0, _X1],
-    /,
+    x0: _X0,
+    f1: _F[_X0, _X1],
+    f2: None = None,
+    f3: _OF[_X2, _X3] = None,
+    f4: _OF[_X3, _X4] = None,
+    f5: _OF[_X4, _X5] = None,
+    f6: _OF[_X5, _X6] = None,
+    f7: _OF[_X6, _X7] = None,
+    f8: _OF[_X7, _X8] = None,
+    f9: _OF[_X8, _X9] = None,
 ) -> _X1: ...
 
 
 @overload
 def pipe(
-    value: _X0,
-    f1: _Morphism[_X0, _X1],
-    f2: _Morphism[_X1, _X2],
-    /,
+    x0: _X0,
+    f1: _F[_X0, _X1],
+    f2: _F[_X1, _X2],
+    f3: None = None,
+    f4: _OF[_X3, _X4] = None,
+    f5: _OF[_X4, _X5] = None,
+    f6: _OF[_X5, _X6] = None,
+    f7: _OF[_X6, _X7] = None,
+    f8: _OF[_X7, _X8] = None,
+    f9: _OF[_X8, _X9] = None,
 ) -> _X2: ...
 
 
 @overload
 def pipe(
-    value: _X0,
-    f1: _Morphism[_X0, _X1],
-    f2: _Morphism[_X1, _X2],
-    f3: _Morphism[_X2, _X3],
-    /,
+    x0: _X0,
+    f1: _F[_X0, _X1],
+    f2: _F[_X1, _X2],
+    f3: _F[_X2, _X3],
+    f4: None = None,
+    f5: _OF[_X4, _X5] = None,
+    f6: _OF[_X5, _X6] = None,
+    f7: _OF[_X6, _X7] = None,
+    f8: _OF[_X7, _X8] = None,
+    f9: _OF[_X8, _X9] = None,
 ) -> _X3: ...
 
 
 @overload
 def pipe(
-    value: _X0,
-    f1: _Morphism[_X0, _X1],
-    f2: _Morphism[_X1, _X2],
-    f3: _Morphism[_X2, _X3],
-    f4: _Morphism[_X3, _X4],
-    /,
+    x0: _X0,
+    f1: _F[_X0, _X1],
+    f2: _F[_X1, _X2],
+    f3: _F[_X2, _X3],
+    f4: _F[_X3, _X4],
+    f5: None = None,
+    f6: _OF[_X5, _X6] = None,
+    f7: _OF[_X6, _X7] = None,
+    f8: _OF[_X7, _X8] = None,
+    f9: _OF[_X8, _X9] = None,
 ) -> _X4: ...
 
 
 @overload
 def pipe(
-    value: _X0,
-    f1: _Morphism[_X0, _X1],
-    f2: _Morphism[_X1, _X2],
-    f3: _Morphism[_X2, _X3],
-    f4: _Morphism[_X3, _X4],
-    f5: _Morphism[_X4, _X5],
-    /,
+    x0: _X0,
+    f1: _F[_X0, _X1],
+    f2: _F[_X1, _X2],
+    f3: _F[_X2, _X3],
+    f4: _F[_X3, _X4],
+    f5: _F[_X4, _X5],
+    f6: None = None,
+    f7: _OF[_X6, _X7] = None,
+    f8: _OF[_X7, _X8] = None,
+    f9: _OF[_X8, _X9] = None,
 ) -> _X5: ...
 
 
 @overload
 def pipe(
-    value: _X0,
-    f1: _Morphism[_X0, _X1],
-    f2: _Morphism[_X1, _X2],
-    f3: _Morphism[_X2, _X3],
-    f4: _Morphism[_X3, _X4],
-    f5: _Morphism[_X4, _X5],
-    f6: _Morphism[_X5, _X6],
-    /,
+    x0: _X0,
+    f1: _F[_X0, _X1],
+    f2: _F[_X1, _X2],
+    f3: _F[_X2, _X3],
+    f4: _F[_X3, _X4],
+    f5: _F[_X4, _X5],
+    f6: _F[_X5, _X6],
+    f7: None = None,
+    f8: _OF[_X7, _X8] = None,
+    f9: _OF[_X8, _X9] = None,
 ) -> _X6: ...
 
 
 @overload
 def pipe(
-    value: _X0,
-    f1: _Morphism[_X0, _X1],
-    f2: _Morphism[_X1, _X2],
-    f3: _Morphism[_X2, _X3],
-    f4: _Morphism[_X3, _X4],
-    f5: _Morphism[_X4, _X5],
-    f6: _Morphism[_X5, _X6],
-    f7: _Morphism[_X6, _X7],
-    /,
+    x0: _X0,
+    f1: _F[_X0, _X1],
+    f2: _F[_X1, _X2],
+    f3: _F[_X2, _X3],
+    f4: _F[_X3, _X4],
+    f5: _F[_X4, _X5],
+    f6: _F[_X5, _X6],
+    f7: _F[_X6, _X7],
+    f8: None = None,
+    f9: _OF[_X8, _X9] = None,
 ) -> _X7: ...
 
 
 @overload
 def pipe(
-    value: _X0,
-    f1: _Morphism[_X0, _X1],
-    f2: _Morphism[_X1, _X2],
-    f3: _Morphism[_X2, _X3],
-    f4: _Morphism[_X3, _X4],
-    f5: _Morphism[_X4, _X5],
-    f6: _Morphism[_X5, _X6],
-    f7: _Morphism[_X6, _X7],
-    f8: _Morphism[_X7, _X8],
-    /,
+    x0: _X0,
+    f1: _F[_X0, _X1],
+    f2: _F[_X1, _X2],
+    f3: _F[_X2, _X3],
+    f4: _F[_X3, _X4],
+    f5: _F[_X4, _X5],
+    f6: _F[_X5, _X6],
+    f7: _F[_X6, _X7],
+    f8: _F[_X7, _X8],
+    f9: None = None,
 ) -> _X8: ...
 
 
 @overload
 def pipe(
-    value: _X0,
-    f1: _Morphism[_X0, _X1],
-    f2: _Morphism[_X1, _X2],
-    f3: _Morphism[_X2, _X3],
-    f4: _Morphism[_X3, _X4],
-    f5: _Morphism[_X4, _X5],
-    f6: _Morphism[_X5, _X6],
-    f7: _Morphism[_X6, _X7],
-    f8: _Morphism[_X7, _X8],
-    f9: _Morphism[_X8, _X9],
-    /,
+    x0: _X0,
+    f1: _F[_X0, _X1],
+    f2: _F[_X1, _X2],
+    f3: _F[_X2, _X3],
+    f4: _F[_X3, _X4],
+    f5: _F[_X4, _X5],
+    f6: _F[_X5, _X6],
+    f7: _F[_X6, _X7],
+    f8: _F[_X7, _X8],
+    f9: _F[_X8, _X9],
 ) -> _X9: ...
 
 
-@overload
-def pipe(
-    value: _X,
-    *fs: _Endomorphism[_X],
-) -> _X: ...
-
-
-def pipe(value: Any, *fs: _Morphism[Any, Any]) -> Any:  # type: ignore[explicit-any]
-    return functools.reduce(lambda acc, f: f(acc), fs, value)
-
-
-@overload
-def compose() -> _Endomorphism[_X0]: ...
-
-
-@overload
-def compose(
-    f1: _Morphism[_X0, _X1],
-    /,
-) -> _Morphism[_X0, _X1]: ...
-
-
-@overload
-def compose(
-    f1: _Morphism[_X0, _X1],
-    f2: _Morphism[_X1, _X2],
-    /,
-) -> _Morphism[_X0, _X2]: ...
-
-
-@overload
-def compose(
-    f1: _Morphism[_X0, _X1],
-    f2: _Morphism[_X1, _X2],
-    f3: _Morphism[_X2, _X3],
-    /,
-) -> _Morphism[_X0, _X3]: ...
-
-
-@overload
-def compose(
-    f1: _Morphism[_X0, _X1],
-    f2: _Morphism[_X1, _X2],
-    f3: _Morphism[_X2, _X3],
-    f4: _Morphism[_X3, _X4],
-    /,
-) -> _Morphism[_X0, _X4]: ...
-
-
-@overload
-def compose(
-    f1: _Morphism[_X0, _X1],
-    f2: _Morphism[_X1, _X2],
-    f3: _Morphism[_X2, _X3],
-    f4: _Morphism[_X3, _X4],
-    f5: _Morphism[_X4, _X5],
-    /,
-) -> _Morphism[_X0, _X5]: ...
-
-
-@overload
-def compose(
-    f1: _Morphism[_X0, _X1],
-    f2: _Morphism[_X1, _X2],
-    f3: _Morphism[_X2, _X3],
-    f4: _Morphism[_X3, _X4],
-    f5: _Morphism[_X4, _X5],
-    f6: _Morphism[_X5, _X6],
-    /,
-) -> _Morphism[_X0, _X6]: ...
-
-
-@overload
-def compose(
-    f1: _Morphism[_X0, _X1],
-    f2: _Morphism[_X1, _X2],
-    f3: _Morphism[_X2, _X3],
-    f4: _Morphism[_X3, _X4],
-    f5: _Morphism[_X4, _X5],
-    f6: _Morphism[_X5, _X6],
-    f7: _Morphism[_X6, _X7],
-    /,
-) -> _Morphism[_X0, _X7]: ...
-
-
-@overload
-def compose(
-    f1: _Morphism[_X0, _X1],
-    f2: _Morphism[_X1, _X2],
-    f3: _Morphism[_X2, _X3],
-    f4: _Morphism[_X3, _X4],
-    f5: _Morphism[_X4, _X5],
-    f6: _Morphism[_X5, _X6],
-    f7: _Morphism[_X6, _X7],
-    f8: _Morphism[_X7, _X8],
-    /,
-) -> _Morphism[_X0, _X8]: ...
-
-
-@overload
-def compose(
-    f1: _Morphism[_X0, _X1],
-    f2: _Morphism[_X1, _X2],
-    f3: _Morphism[_X2, _X3],
-    f4: _Morphism[_X3, _X4],
-    f5: _Morphism[_X4, _X5],
-    f6: _Morphism[_X5, _X6],
-    f7: _Morphism[_X6, _X7],
-    f8: _Morphism[_X7, _X8],
-    f9: _Morphism[_X8, _X9],
-    /,
-) -> _Morphism[_X0, _X9]: ...
-
-
-@overload
-def compose(*fs: _Endomorphism[_X]) -> _Endomorphism[_X]: ...
-
-
-def compose(*fs: _Morphism[Any, Any]) -> _Morphism[Any, Any]:  # type: ignore[explicit-any]
-    return lambda value: pipe(value, *fs)
+def pipe(  # noqa: PLR0911, PLR0913
+    x0: _X0,
+    f1: _OF[_X0, _X1] = None,
+    f2: _OF[_X1, _X2] = None,
+    f3: _OF[_X2, _X3] = None,
+    f4: _OF[_X3, _X4] = None,
+    f5: _OF[_X4, _X5] = None,
+    f6: _OF[_X5, _X6] = None,
+    f7: _OF[_X6, _X7] = None,
+    f8: _OF[_X7, _X8] = None,
+    f9: _OF[_X8, _X9] = None,
+) -> _X0 | _X1 | _X2 | _X3 | _X4 | _X5 | _X6 | _X7 | _X8 | _X9:
+    if f1 is None:
+        return x0
+    if f2 is None:
+        return f1(x0)
+    if f3 is None:
+        return f2(f1(x0))
+    if f4 is None:
+        return f3(f2(f1(x0)))
+    if f5 is None:
+        return f4(f3(f2(f1(x0))))
+    if f6 is None:
+        return f5(f4(f3(f2(f1(x0)))))
+    if f7 is None:
+        return f6(f5(f4(f3(f2(f1(x0))))))
+    if f8 is None:
+        return f7(f6(f5(f4(f3(f2(f1(x0)))))))
+    if f9 is None:
+        return f8(f7(f6(f5(f4(f3(f2(f1(x0))))))))
+    return f9(f8(f7(f6(f5(f4(f3(f2(f1(x0)))))))))
--- tests/trcks/fp/test_composition.py
+++ tests/trcks/fp/test_composition.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import sys
 from collections.abc import Callable
 
@@ -10,72 +12,163 @@ if sys.version_info >= (3, 11):
 else:
     from typing_extensions import assert_type
 
-_REPETITIONS = [1, 2, 3, 4, 5, 10, 20, 100]
+
+def _foo(x: int) -> str:
+    return f"Foo: {x + 1}"
 
 
-@pytest.mark.parametrize("input_", [1, "a", [1, 2, 3], {"a": 1}])
-def test_compose_without_arguments_returns_identity_function(input_: object) -> None:
-    func: Callable[[object], object] = compose()
-    output = func(input_)
-    assert output is input_
+def _incr(x: int) -> int:
+    return x + 1
+
+
+_ZERO_INCRS = ()
+_ONE_INCR = (_incr,)
+_TWO_INCRS = (_incr, _incr)
+_THREE_INCRS = (_incr, _incr, _incr)
+_FOUR_INCRS = (_incr, _incr, _incr, _incr)
+_FIVE_INCRS = (_incr, _incr, _incr, _incr, _incr)
+_SIX_INCRS = (_incr, _incr, _incr, _incr, _incr, _incr)
+_SEVEN_INCRS = (_incr, _incr, _incr, _incr, _incr, _incr, _incr)
+_EIGHT_INCRS = (_incr, _incr, _incr, _incr, _incr, _incr, _incr, _incr)
+_NINE_INCRS = (_incr, _incr, _incr, _incr, _incr, _incr, _incr, _incr, _incr)
+
+
+def test_compose_correctly_composes_1_function() -> None:
+    composed = compose(*_ONE_INCR)
+    assert_type(composed, Callable[[int], int])
+    assert composed(0) == len(_ONE_INCR)
+
+
+def test_compose_correctly_composes_2_functions() -> None:
+    composed = compose(*_TWO_INCRS)
+    assert_type(composed, Callable[[int], int])
+    assert composed(0) == len(_TWO_INCRS)
+
+
+def test_compose_correctly_composes_3_functions() -> None:
+    composed = compose(*_THREE_INCRS)
+    assert_type(composed, Callable[[int], int])
+    assert composed(0) == len(_THREE_INCRS)
+
+
+def test_compose_correctly_composes_4_functions() -> None:
+    composed = compose(*_FOUR_INCRS)
+    assert_type(composed, Callable[[int], int])
+    assert composed(0) == len(_FOUR_INCRS)
+
+
+def test_compose_correctly_composes_5_functions() -> None:
+    composed = compose(*_FIVE_INCRS)
+    assert_type(composed, Callable[[int], int])
+    assert composed(0) == len(_FIVE_INCRS)
+
+
+def test_compose_correctly_composes_6_functions() -> None:
+    composed = compose(*_SIX_INCRS)
+    assert_type(composed, Callable[[int], int])
+    assert composed(0) == len(_SIX_INCRS)
+
+
+def test_compose_correctly_composes_7_functions() -> None:
+    composed = compose(*_SEVEN_INCRS)
+    assert_type(composed, Callable[[int], int])
+    assert composed(0) == len(_SEVEN_INCRS)
+
+
+def test_compose_correctly_composes_8_functions() -> None:
+    composed = compose(*_EIGHT_INCRS)
+    assert_type(composed, Callable[[int], int])
+    assert composed(0) == len(_EIGHT_INCRS)
+
+
+def test_compose_correctly_composes_9_functions() -> None:
+    composed = compose(*_NINE_INCRS)
+    assert_type(composed, Callable[[int], int])
+    assert composed(0) == len(_NINE_INCRS)
 
 
 @pytest.mark.parametrize("value", [23, 42, 100, -1, 0, 1])
-def test_compose_with_one_argument_returns_equivalent_function(value: int) -> None:
-    def foo(x: int) -> str:
-        return f"Foo: {x + 1}"
-
-    composed_foo = compose(foo)
-    assert_type(composed_foo, Callable[[int], str])
-    assert composed_foo(value) == foo(value)
+def test_compose_with_1_argument_returns_equivalent_function(value: int) -> None:
+    composed = compose(_foo)
+    assert_type(composed, Callable[[int], str])
+    assert composed(value) == _foo(value)
 
 
 @pytest.mark.parametrize("value", [0, 1, -1, 10, 100, 1000])
-def test_compose_with_two_arguments_returns_composed_function(value: int) -> None:
-    def foo(x: int) -> str:
-        return f"Foo: {x + 1}"
-
-    def bar(x: str) -> int:
-        return len(x)
-
-    foo_then_bar = compose(foo, bar)
-    assert_type(foo_then_bar, Callable[[int], int])
-    assert foo_then_bar(value) == bar(foo(value))
-
-
-@pytest.mark.parametrize("repetitions", _REPETITIONS)
-def test_compose_correctly_composes_many_functions(repetitions: int) -> None:
-    def incr(x: int) -> int:
-        return x + 1
-
-    functions = (incr,) * repetitions
-    composed = compose(*functions)
+def test_compose_with_2_arguments_returns_composed_function(value: int) -> None:
+    composed = compose(_foo, len)
     assert_type(composed, Callable[[int], int])
-    assert composed(0) == repetitions
+    assert composed(value) == len(_foo(value))
+
+
+def test_pipe_correctly_applies_0_functions() -> None:
+    piped = pipe(0, *_ZERO_INCRS)
+    assert_type(piped, int)
+    assert piped == len(_ZERO_INCRS)
+
+
+def test_pipe_correctly_applies_1_function() -> None:
+    piped = pipe(0, *_ONE_INCR)
+    assert_type(piped, int)
+    assert piped == len(_ONE_INCR)
+
+
+def test_pipe_correctly_applies_2_functions() -> None:
+    piped = pipe(0, *_TWO_INCRS)
+    assert_type(piped, int)
+    assert piped == len(_TWO_INCRS)
+
+
+def test_pipe_correctly_applies_3_functions() -> None:
+    piped = pipe(0, *_THREE_INCRS)
+    assert_type(piped, int)
+    assert piped == len(_THREE_INCRS)
+
+
+def test_pipe_correctly_applies_4_functions() -> None:
+    piped = pipe(0, *_FOUR_INCRS)
+    assert_type(piped, int)
+    assert piped == len(_FOUR_INCRS)
+
+
+def test_pipe_correctly_applies_5_functions() -> None:
+    piped = pipe(0, *_FIVE_INCRS)
+    assert_type(piped, int)
+    assert piped == len(_FIVE_INCRS)
+
+
+def test_pipe_correctly_applies_6_functions() -> None:
+    piped = pipe(0, *_SIX_INCRS)
+    assert_type(piped, int)
+    assert piped == len(_SIX_INCRS)
+
+
+def test_pipe_correctly_applies_7_functions() -> None:
+    piped = pipe(0, *_SEVEN_INCRS)
+    assert_type(piped, int)
+    assert piped == len(_SEVEN_INCRS)
+
+
+def test_pipe_correctly_applies_8_functions() -> None:
+    piped = pipe(0, *_EIGHT_INCRS)
+    assert_type(piped, int)
+    assert piped == len(_EIGHT_INCRS)
+
+
+def test_pipe_correctly_applies_9_functions() -> None:
+    piped = pipe(0, *_NINE_INCRS)
+    assert_type(piped, int)
+    assert piped == len(_NINE_INCRS)
 
 
 @pytest.mark.parametrize(
     "input_", [42, "test", [4, 5, 6], {"key": "value"}, None, True]
 )
-def test_pipe_with_one_argument_returns_identical_value(input_: object) -> None:
+def test_pipe_with_1_argument_returns_identical_value(input_: object) -> None:
     output = pipe(input_)
     assert output is input_
 
 
 @pytest.mark.parametrize("value", [23, 42, -100, 0, 1000, 999999])
-def test_pipe_with_two_arguments_applies_function_to_value(value: int) -> None:
-    def foo(x: int) -> str:
-        return f"Foo: {x + 1}"
-
-    assert pipe(value, foo) == foo(value)
-
-
-@pytest.mark.parametrize("repetitions", _REPETITIONS)
-def test_pipe_correctly_applies_many_functions(repetitions: int) -> None:
-    def incr(x: int) -> int:
-        return x + 1
-
-    functions = (incr,) * repetitions
-    piped = pipe(0, *functions)
-    assert_type(piped, int)
-    assert piped == repetitions
+def test_pipe_with_2_arguments_applies_function_to_value(value: int) -> None:
+    assert pipe(value, _foo) == _foo(value)
