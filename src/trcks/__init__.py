95b2b933713b0bff34bab163b9dfcba9c00b71c7
Christoph Gietl <christophgietl@users.noreply.github.com>
2025-03-08 12:37:53 +0100

    Document main package

--- src/trcks/__init__.py
+++ src/trcks/__init__.py
@@ -1,3 +1,15 @@
+"""Typesafe railway oriented programming (ROP).
+
+This package provides
+
+- the generic (return) types `trcks.Result` and `trcks.AwaitableResult` and
+- the subpackages `trcks.fp` and `trcks.oop` for working with these types
+  in a functional and object-oriented way, respectively.
+
+See:
+    https://fsharpforfunandprofit.com/posts/recipe-part2/
+"""
+
 from trcks._type_aliases import (
     AwaitableFailure,
     AwaitableResult,
