37c8d4f5f81336493be03c986b39556f1151715d
Christoph Gietl <christophgietl@users.noreply.github.com>
2025-03-19 09:39:01 +0100

    Refactor test_composition.py

--- tests/trcks/fp/test_composition.py
+++ tests/trcks/fp/test_composition.py
@@ -2,15 +2,28 @@ from __future__ import annotations
 
 import sys
 from collections.abc import Callable
+from typing import TypeVar
 
 import pytest
 
-from trcks.fp.composition import compose, pipe
+from trcks.fp.composition import Composable, Pipeline, compose, pipe
 
 if sys.version_info >= (3, 13):
     from typing import assert_type
 else:
     from typing_extensions import assert_type
+
+if sys.version_info >= (3, 11):
+    from typing import TypeAlias
+else:
+    from typing_extensions import TypeAlias
+
+_T = TypeVar("_T")
+
+_IntComposable: TypeAlias = Composable[int, int, int, int, int, int, int, int]
+_IntPipeline: TypeAlias = Pipeline[int, int, int, int, int, int, int, int]
+_Tuple7: TypeAlias = tuple[_T, _T, _T, _T, _T, _T, _T]
+_Tuple8: TypeAlias = tuple[_T, _T, _T, _T, _T, _T, _T, _T]
 
 
 def _foo(x: int) -> str:
@@ -21,58 +34,33 @@ def _incr(x: int) -> int:
     return x + 1
 
 
-_ZERO_INCRS = ()
-_ONE_INCR = (_incr,)
-_TWO_INCRS = (_incr, _incr)
-_THREE_INCRS = (_incr, _incr, _incr)
-_FOUR_INCRS = (_incr, _incr, _incr, _incr)
-_FIVE_INCRS = (_incr, _incr, _incr, _incr, _incr)
-_SIX_INCRS = (_incr, _incr, _incr, _incr, _incr, _incr)
-_SEVEN_INCRS = (_incr, _incr, _incr, _incr, _incr, _incr, _incr)
-_EIGHT_INCRS = (_incr, _incr, _incr, _incr, _incr, _incr, _incr, _incr)
-_NINE_INCRS = (_incr, _incr, _incr, _incr, _incr, _incr, _incr, _incr, _incr)
+_COMPOSABLES: _Tuple7[_IntComposable] = (
+    (_incr,),
+    (_incr, _incr),
+    (_incr, _incr, _incr),
+    (_incr, _incr, _incr, _incr),
+    (_incr, _incr, _incr, _incr, _incr),
+    (_incr, _incr, _incr, _incr, _incr, _incr),
+    (_incr, _incr, _incr, _incr, _incr, _incr, _incr),
+)
+
+_PIPELINES: _Tuple8[_IntPipeline] = (
+    (0,),
+    (0, *_COMPOSABLES[0]),
+    (0, *_COMPOSABLES[1]),
+    (0, *_COMPOSABLES[2]),
+    (0, *_COMPOSABLES[3]),
+    (0, *_COMPOSABLES[4]),
+    (0, *_COMPOSABLES[5]),
+    (0, *_COMPOSABLES[6]),
+)
 
 
-def test_compose_correctly_composes_1_function() -> None:
-    composed = compose(_ONE_INCR)
+@pytest.mark.parametrize("composable", _COMPOSABLES)
+def test_compose_correctly_composes_composable(composable: _IntComposable) -> None:
+    composed = compose(composable)
     assert_type(composed, Callable[[int], int])
-    assert composed(0) == len(_ONE_INCR)
-
-
-def test_compose_correctly_composes_2_functions() -> None:
-    composed = compose(_TWO_INCRS)
-    assert_type(composed, Callable[[int], int])
-    assert composed(0) == len(_TWO_INCRS)
-
-
-def test_compose_correctly_composes_3_functions() -> None:
-    composed = compose(_THREE_INCRS)
-    assert_type(composed, Callable[[int], int])
-    assert composed(0) == len(_THREE_INCRS)
-
-
-def test_compose_correctly_composes_4_functions() -> None:
-    composed = compose(_FOUR_INCRS)
-    assert_type(composed, Callable[[int], int])
-    assert composed(0) == len(_FOUR_INCRS)
-
-
-def test_compose_correctly_composes_5_functions() -> None:
-    composed = compose(_FIVE_INCRS)
-    assert_type(composed, Callable[[int], int])
-    assert composed(0) == len(_FIVE_INCRS)
-
-
-def test_compose_correctly_composes_6_functions() -> None:
-    composed = compose(_SIX_INCRS)
-    assert_type(composed, Callable[[int], int])
-    assert composed(0) == len(_SIX_INCRS)
-
-
-def test_compose_correctly_composes_7_functions() -> None:
-    composed = compose(_SEVEN_INCRS)
-    assert_type(composed, Callable[[int], int])
-    assert composed(0) == len(_SEVEN_INCRS)
+    assert composed(0) == len(composable)
 
 
 @pytest.mark.parametrize("value", [23, 42, 100, -1, 0, 1])
@@ -89,52 +77,11 @@ def test_compose_with_2_arguments_returns_composed_function(value: int) -> None:
     assert composed(value) == len(_foo(value))
 
 
-def test_pipe_correctly_applies_0_functions() -> None:
-    piped = pipe((0, *_ZERO_INCRS))
+@pytest.mark.parametrize("p", _PIPELINES)
+def test_pipe_correctly_applies_pipeline(p: _IntPipeline) -> None:
+    piped = pipe(p)
     assert_type(piped, int)
-    assert piped == len(_ZERO_INCRS)
-
-
-def test_pipe_correctly_applies_1_function() -> None:
-    piped = pipe((0, *_ONE_INCR))
-    assert_type(piped, int)
-    assert piped == len(_ONE_INCR)
-
-
-def test_pipe_correctly_applies_2_functions() -> None:
-    piped = pipe((0, *_TWO_INCRS))
-    assert_type(piped, int)
-    assert piped == len(_TWO_INCRS)
-
-
-def test_pipe_correctly_applies_3_functions() -> None:
-    piped = pipe((0, *_THREE_INCRS))
-    assert_type(piped, int)
-    assert piped == len(_THREE_INCRS)
-
-
-def test_pipe_correctly_applies_4_functions() -> None:
-    piped = pipe((0, *_FOUR_INCRS))
-    assert_type(piped, int)
-    assert piped == len(_FOUR_INCRS)
-
-
-def test_pipe_correctly_applies_5_functions() -> None:
-    piped = pipe((0, *_FIVE_INCRS))
-    assert_type(piped, int)
-    assert piped == len(_FIVE_INCRS)
-
-
-def test_pipe_correctly_applies_6_functions() -> None:
-    piped = pipe((0, *_SIX_INCRS))
-    assert_type(piped, int)
-    assert piped == len(_SIX_INCRS)
-
-
-def test_pipe_correctly_applies_7_functions() -> None:
-    piped = pipe((0, *_SEVEN_INCRS))
-    assert_type(piped, int)
-    assert piped == len(_SEVEN_INCRS)
+    assert piped == len(p) - 1
 
 
 @pytest.mark.parametrize(
