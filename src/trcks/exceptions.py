67be70b0e0dbe76a7b0b1d60f2a28e1994a60495
Christoph Gietl <christophgietl@users.noreply.github.com>
2026-06-22 22:45:41 +0200

    Turn TrcksTypeError into a data class

--- src/trcks/exceptions.py
+++ src/trcks/exceptions.py
@@ -3,7 +3,8 @@
 from __future__ import annotations
 
 from collections.abc import Sized
-from typing import TYPE_CHECKING
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Final
 
 if TYPE_CHECKING:
     from trcks._typing import Self
@@ -11,10 +12,14 @@ if TYPE_CHECKING:
 __docformat__ = "google"
 
 
+@dataclass(eq=False, repr=False, slots=True)
 class TrcksTypeError(TypeError):
     """Raised when [trcks][] functions are called with an argument of wrong type.
 
-    Contains information about the offending object.
+    Attributes:
+        offending_object_class: Class of the object that caused the error.
+        offending_object_length: Length of the object that caused the error.
+        expected_type_name: Name of the expected type.
 
     Example:
         >>> err = TrcksTypeError(int, None, "Pipeline")
@@ -28,37 +33,20 @@ class TrcksTypeError(TypeError):
         'Pipeline'
     """
 
-    __slots__: tuple[str, ...] = (
-        "_expected_type_name",
-        "_offending_object_class",
-        "_offending_object_length",
-    )
+    offending_object_class: Final[type]
+    offending_object_length: Final[int | None]
+    expected_type_name: Final[str]
 
-    def __init__(
-        self,
-        offending_object_class: type,
-        offending_object_length: int | None,
-        expected_type_name: str,
-    ) -> None:
-        """Initialize error from offending object information and the expected type.
-
-        Args:
-            offending_object_class: Class of the object that caused the error.
-            offending_object_length: Length of the object that caused the error.
-            expected_type_name: Name of the expected type.
-        """
-        self._offending_object_class: type = offending_object_class
-        self._offending_object_length: int | None = offending_object_length
-        self._expected_type_name: str = expected_type_name
-
-        prefix = f"object of type '{self._offending_object_class.__name__}' "
-        if self._offending_object_length is None:
+    def __post_init__(self) -> None:
+        """Post-init processing for [dataclasses.dataclass][]."""
+        prefix = f"object of type '{self.offending_object_class.__name__}' "
+        if self.offending_object_length is None:
             infix = ""
         else:
-            infix = f"and length {self._offending_object_length} "
-        suffix = f"is not a valid '{self._expected_type_name}'"
+            infix = f"and length {self.offending_object_length} "
+        suffix = f"is not a valid '{self.expected_type_name}'"
 
-        super().__init__(f"{prefix}{infix}{suffix}")
+        super(TrcksTypeError, self).__init__(f"{prefix}{infix}{suffix}")
 
     @classmethod
     def construct_from_offending_object(
@@ -109,18 +97,3 @@ class TrcksTypeError(TypeError):
             ),
             expected_type_name=expected_type_name,
         )
-
-    @property
-    def expected_type_name(self) -> str:
-        """Name of the expected type."""
-        return self._expected_type_name
-
-    @property
-    def offending_object_class(self) -> type:
-        """Class of the object that caused the error."""
-        return self._offending_object_class
-
-    @property
-    def offending_object_length(self) -> int | None:
-        """Length of the object that caused the error."""
-        return self._offending_object_length
